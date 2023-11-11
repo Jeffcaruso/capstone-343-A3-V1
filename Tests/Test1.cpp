830571462fcaa7d3af55b79adcfe8088e80f30c0
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-11-10 22:07:07 -0800

    most, but not all of test 1 yet...

--- Tests/Test1.cpp
+++ Tests/Test1.cpp
@@ -21,19 +21,21 @@ using namespace std;
  * by creating a singleton class with our visitor functions
  * stringstream SS contains the output from visitor
  */
-class Tester {
+class Tester
+{
 public:
-  Tester() = delete;
-  // insert output to SS rather than cout, so we can test it
-  static stringstream SS;
-  static string getSs() { return SS.str(); }
-  static void resetSs() { SS.str(string()); }
-  // visitor function used for DFS and BFS
-  static void labelVisitor(const string &Label) { SS << Label; }
-  // visitor function used for edges for minimum spanning tree
-  static void edgeVisitor(const string &From, const string &To, int Weight) {
-    SS << "[" << From << To << " " << Weight << "]";
-  }
+	Tester() = delete;
+	// insert output to SS rather than cout, so we can test it
+	static stringstream SS;
+	static string getSs() { return SS.str(); }
+	static void resetSs() { SS.str(string()); }
+	// visitor function used for DFS and BFS
+	static void labelVisitor(const string &Label) { SS << Label; }
+	// visitor function used for edges for minimum spanning tree
+	static void edgeVisitor(const string &From, const string &To, int Weight)
+	{
+		SS << "[" << From << To << " " << Weight << "]";
+	}
 };
 
 // initialize the static variable
@@ -42,65 +44,52 @@ stringstream Tester::SS;
 
 // convert a map to a string so we can compare it
 template <typename K, typename L>
-static string map2string(const map<K, L> &Mp) {
-  stringstream Out;
-  for (auto &P : Mp)
-    Out << "[" << P.first << ":" << P.second << "]";
-  return Out.str();
+static string map2string(const map<K, L> &Mp)
+{
+	stringstream Out;
+	for (auto &P : Mp)
+		Out << "[" << P.first << ":" << P.second << "]";
+	return Out.str();
 }
-
-
 
 TEST(Test1, BasicGraphTesting)
 {
 	Graph G(true);
 
-  //maybe add
-  EXPECT_TRUE(G.add("a")); //add vertext a
-  EXPECT_TRUE(G.add("b")); //add vertext b
-  EXPECT_TRUE(G.add("c")); //add vertext c
-  EXPECT_TRUE(G.add("d")); //add vertext d
-  EXPECT_TRUE(G.add("e")); //add vertext e
+	//add edges a-e to Graph G
+	//all should pass, returning true
+	EXPECT_TRUE(G.add("a")); // add vertext a
+	EXPECT_TRUE(G.add("b")); // add vertext b
+	EXPECT_TRUE(G.add("c")); // add vertext c
+	EXPECT_TRUE(G.add("d")); // add vertext d
+	EXPECT_TRUE(G.add("e")); // add vertext e
+
+	//dup adds, connections being made, etc...
+	EXPECT_FALSE(G.add("b")); // b added twice
+	EXPECT_TRUE(G.connect("a", "b", 10)); // connect a to b
+	EXPECT_FALSE(G.connect("a", "b", 50)); // duplicate connect a to b
+	EXPECT_FALSE(G.connect("a", "a", 1)); // connect a to itself
+
+	//connect w/o testing, just get the rest done.
+	G.connect("a", "d", 40); // connect a to d, cost 40
+	G.connect("a", "c", 20); // connect a to c, cost 20
+
+	//Testing methods for Graph data (and metadata) access
+	EXPECT_EQ(G.verticesSize(), 5); // graph number of vertices
+	EXPECT_EQ(G.edgesSize(), 3); // graph number of edges
+	//test neighbors size
+	EXPECT_EQ(G.neighborsSize("a"), 3); // vertex number of edges
+	EXPECT_EQ(G.neighborsSize("c") == 0); // no outgoing edges c
+	EXPECT_EQ(G.neighborsSize("xxx"), -1); // no edges for xxx
+	//test contains
+	EXPECT_FALSE(G.contains("xxx")); // xxx not in graph
+	EXPECT_TRUE(G.contains("a")); // a in graph
+
+	// check that they are sorted based on edge end label	
+	string expectedAns = "b(10),c(20),d(40)";
+	EXPECT_EQ(G.getEdgesAsString("a"), expectedAns);
 
 
-// alt approach (prior, did not work)
-	// EXPECT_EQ(G.add("a") , "add vertex a");
-	// EXPECT_EQ(G.add("b") , "add vertex b");
-	// EXPECT_EQ(G.add("c") , "add vertex c");
-	// EXPECT_EQ(G.add("d") , "add vertex d");
-	// EXPECT_EQ(G.add("e") , "add vertex e");
+	// disconnect (non-)existent edge/vertex
 
-//Pisan setup...
-//   Graph G;
-//   assert(G.add("a") && "add vertex a");
-//   assert(G.add("b") && "add vertex b");
-//   assert(G.add("c") && "add vertex c");
-//   assert(G.add("d") && "add vertex d");
-//   assert(G.add("e") && "add vertex e");
-
-
-
-//   assert(!G.add("b") && "b added twice");
-//   assert(G.connect("a", "b", 10) && "connect a b");
-//   assert(!G.connect("a", "b", 50) && "duplicate connect a b");
-//   assert(!G.connect("a", "a", 1) && "connect a to itself");
-//   G.connect("a", "d", 40);
-//   G.connect("a", "c", 20);
-//   assert((G.verticesSize() == 5) && "graph number of vertices");
-//   assert((G.edgesSize() == 3) && "graph number of edges");
-//   assert((G.neighborsSize("a") == 3) && "vertex number of edges");
-//   assert((G.neighborsSize("c") == 0) && "no outgoing edges c");
-//   assert((G.neighborsSize("xxx") == -1) && "no edges for xxx");
-//   assert(!G.contains("xxx") && "xxx not in graph");
-//   assert(G.contains("a") && "a in graph");
-
-//   // check that they are sorted based on edge end label
-//   assert(G.getEdgesAsString("a") == "b(10),c(20),d(40)");
-//   // disconnect non-existent edge/vertex
-//   assert(!G.disconnect("a", "e") && "disconnecting non-existent vertex");
-//   assert((G.edgesSize() == 3) && "disconnected nonexisting");
-//   assert(G.disconnect("a", "c") && "a-c disconnect");
-//   assert((G.edgesSize() == 2) && "number of edges after disconnect");
-//   assert((G.neighborsSize("a") == 2) && "a has 2 edges");
-//   assert(G.getEdgesAsString("a") == "b(10),d(40)" && "removing middle edge");
 }
