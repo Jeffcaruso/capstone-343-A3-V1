39581a7f78a39f608817bedefffee7a2a8172bea
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-11-11 19:27:23 -0800

    Test 3 *SD*
    Should be ok, although, I'm not too sure what's going on with calling BFS first time then DFS every future time...

--- Tests/Test3.cpp
+++ Tests/Test3.cpp
@@ -1,123 +1,102 @@
 //============================================================================
 // Name        : Test3
-// Test Desc.  : Testing traversal methods on BSTs
-//				 	(coverage for old test 3 method)
+// Test Desc.  : testing Breadth First Search (BFS) on graph 0
+//				 	(coverage for old testGraph0BFS(). was #2 test method...)
 // Author      : Jeffrey Caruso, Yusuf Pisan
 // Date    	   : Fall 2023
 //============================================================================
 
 #include <gtest/gtest.h>
-#include "applib/bst.h"
+#include "applib/graph.h"
 
 #include <iostream>
 #include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
-
-
-/**
- * Testing BST - Binary Search Tree functions
- *
- * This file has series of tests for BST
- * Each test is independent and uses assert statements
- * Test functions are of the form
- *
- *      test_netidXX()
- *
- * where netid is UW netid and XX is the test number starting from 01
- *
- * Test functions can only use the public functions from BST
- * testBSTAll() is called from main in main.cpp
- * testBSTAll calls all other functions
- * @author Multiple
- * @date ongoing
- */
 
 /**
  * Trying to avoid global variables,
  * by creating a singleton class with our visitor functions
  * stringstream SS contains the output from visitor
  */
-class TreeVisitor {
+class Tester
+{
 public:
-  // never create an instance of TreeVisitor object
-  // we'll just use the static functions
-  TreeVisitor() = delete;
-
-  // insert output to SS rather than cout, so we can test it
-  static stringstream SS;
-
-  // get SS as a string
-  static string getSS() { return SS.str(); }
-
-  // set SS to be empty string
-  static void resetSS() { SS.str(string()); }
-
-  // instead of cout, insert item into SS, a stringstream object
-  static void visitor(const string &Item) { SS << Item; }
-
-  // instead of cout, insert item into SS, a stringstream object
-  static void visitor(const int &Item) { SS << Item; }
+	Tester() = delete;
+	// insert output to SS rather than cout, so we can test it
+	static stringstream SS;
+	static string getSs() { return SS.str(); }
+	static void resetSs() { SS.str(string()); }
+	// visitor function used for DFS and BFS
+	static void labelVisitor(const string &Label) { SS << Label; }
+	// visitor function used for edges for minimum spanning tree
+	static void edgeVisitor(const string &From, const string &To, int Weight)
+	{
+		SS << "[" << From << To << " " << Weight << "]";
+	}
 };
 
 // initialize the static variable
-//  warning: initialization of 'SS' with static storage duration
-//  may throw an exception that cannot be caught [cert-err58-cpp]
-//  Not sure how to do it without making code harder to read
-//  NOLINTNEXTLINE
-stringstream TreeVisitor::SS;
+// NOLINTNEXTLINE
+stringstream Tester::SS;
 
-template <class T> void visitorSimple(const T &Item) {
-  cout << "visitorSimple: " << Item;
+// convert a map to a string so we can compare it
+template <typename K, typename L>
+static string map2string(const map<K, L> &Mp)
+{
+	stringstream Out;
+	for (auto &P : Mp)
+		Out << "[" << P.first << ":" << P.second << "]";
+	return Out.str();
 }
 
+/*
+ * Note, if testing is not working as expected, there is a decent chance of some degree of misconfigurations.
+ * I.E.   assert(G.contains("A") && "a in graph"); -> EXPECT_TRUE(G.contains("A")); // a in graph
+ * 				May not be as correct of an interpretation as I am expecting...
+ *
+ */
 
-
-// Testing traversal
-TEST(Test3, testBSTTraversal)
+// detailed == testing
+TEST(Test2, Graph0BFS)
 {
-	BST<string> B1;
-	BST<string> B2;
-	BST<string> B3;
-	
-	//add to B1
-    for (auto &S : vector<string>{"c", "a", "f", "g", "x"})
-    	B1.add(S);
+	//prep graph, read in graph0.txt
+	Graph G;
+	if (!G.readFile("graph0.txt"))
+		return;
 
-	//add to B2
-    for (auto &S : vector<string>{"c", "f", "a", "g", "x"})
-    	B2.add(S);
+	/// ***
+	//// NEED TO CHECK IN ON USING BFS first time, then DFS the rest of the time...
+	/// ***
 
-	//add to B3
-    B3.add("b");
 
-	//reset StringStream to ""
-	TreeVisitor::resetSS();
-	B1.inOrderTraverse(TreeVisitor::visitor);
-	string expectedResult = "acfgx";
-	// check that SS matches expected Result
-	EXPECT_EQ(TreeVisitor::getSS(), expectedResult);
+	//A
+	Tester::resetSs();
+	string ansStr = "A";
+	G.bfs(ansStr, Tester::labelVisitor);
+	ansStr = "ABC";
+	EXPECT_EQ(Tester::getSs(), ansStr); // starting from A
 
-	// testing out simpleVisitor to demonstrate
-	// any function that has a matching signature can be called
-	B1.inOrderTraverse(visitorSimple);
+	//B
+	Tester::resetSs()
+	string ansStr = "B";
+	G.dfs(ansStr, Tester::labelVisitor);
+	ansStr = "BC";
+	EXPECT_EQ(Tester::getSs(), ansStr); // starting from B
 
-	//reset SS
-	TreeVisitor::resetSS();
-	//do a preorder traversal
-	B1.preOrderTraverse(TreeVisitor::visitor);
-	//set expected result to new expected result...
-	expectedResult = "cafgx";
-	// check that SS matches expected Result
-	EXPECT_EQ(TreeVisitor::getSS(), expectedResult);
+	//C
+	Tester::resetSs();
+	string ansStr = "C";
+	G.dfs(ansStr, Tester::labelVisitor);
+	ansStr = "C";
+	EXPECT_EQ(Tester::getSs(), ansStr); // starting from C
 
-	TreeVisitor::resetSS();
-	B1.postOrderTraverse(TreeVisitor::visitor);
-	expectedResult = "axgfc";
-	// check that SS matches expected Result
-	EXPECT_EQ(TreeVisitor::getSS(), expectedResult);
+	//X
+	Tester::resetSs();
+	string ansStr = "X";
+	G.dfs(ansStr, Tester::labelVisitor);
+	EXPECT_TRUE(Tester::getSs().empty()); // starting from X
 
-	// visual check of B1
-	cout << "Visual check B1:" << endl;
-	cout << B1 << endl;
 }
