0aaa57e96b26e4dbad8ba9f8833dd497a95bf1da
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-11-11 21:30:00 -0800

    T5 (first 1/2 seems at least mostly done)
    Will do a test run, can see if there are any issues in the first part written...

--- Tests/Test5.cpp
+++ Tests/Test5.cpp
@@ -24,18 +24,18 @@ using namespace std;
 class Tester
 {
 public:
-	Tester() = delete;
-	// insert output to SS rather than cout, so we can test it
-	static stringstream SS;
-	static string getSs() { return SS.str(); }
-	static void resetSs() { SS.str(string()); }
-	// visitor function used for DFS and BFS
-	static void labelVisitor(const string &Label) { SS << Label; }
-	// visitor function used for edges for minimum spanning tree
-	static void edgeVisitor(const string &From, const string &To, int Weight)
-	{
-		SS << "[" << From << To << " " << Weight << "]";
-	}
+    Tester() = delete;
+    // insert output to SS rather than cout, so we can test it
+    static stringstream SS;
+    static string getSs() { return SS.str(); }
+    static void resetSs() { SS.str(string()); }
+    // visitor function used for DFS and BFS
+    static void labelVisitor(const string &Label) { SS << Label; }
+    // visitor function used for edges for minimum spanning tree
+    static void edgeVisitor(const string &From, const string &To, int Weight)
+    {
+        SS << "[" << From << To << " " << Weight << "]";
+    }
 };
 
 // initialize the static variable
@@ -46,10 +46,10 @@ stringstream Tester::SS;
 template <typename K, typename L>
 static string map2string(const map<K, L> &Mp)
 {
-	stringstream Out;
-	for (auto &P : Mp)
-		Out << "[" << P.first << ":" << P.second << "]";
-	return Out.str();
+    stringstream Out;
+    for (auto &P : Mp)
+        Out << "[" << P.first << ":" << P.second << "]";
+    return Out.str();
 }
 
 /*
@@ -60,69 +60,89 @@ static string map2string(const map<K, L> &Mp)
  */
 
 
-
-//Testing Graph0 with non-directed links
+// Testing Graph0 with non-directed links
 TEST(Test5, testGraph0NotDirected)
 {
-	//trivial 'test' to see if the gTest connection is setup...
+    // trivial 'test' to see if the gTest connection is setup...
 
-//   cout << "testGraph0NotDirected" << endl;
-//   bool IsDirectional = false;
-//   Graph G(IsDirectional);
-//   if (!G.readFile("graph0.txt"))
-//     return;
+    //   cout << "testGraph0NotDirected" << endl;
+    //prep non-directional graph
+    bool IsDirectional = false;
+    Graph G(IsDirectional);
+    if (!G.readFile("graph0.txt"))
+        return;
 
-//   Tester::resetSs();
-//   G.bfs("A", Tester::labelVisitor);
-//   assert(Tester::getSs() == "ABC" && "starting from A");
-
-//   Tester::resetSs();
-//   G.dfs("B", Tester::labelVisitor);
-//   assert(Tester::getSs() == "BAC" && "starting from B");
-
-//   Tester::resetSs();
-//   G.dfs("C", Tester::labelVisitor);
-//   assert(Tester::getSs() == "CAB" && "starting from C");
-
-//   Tester::resetSs();
-//   G.dfs("X", Tester::labelVisitor);
-//   assert(Tester::getSs().empty() && "starting from X");
-
-//   map<string, int> Weights;
-//   map<string, string> Previous;
-//   tie(Weights, Previous) = G.dijkstra("A");
-//   // cout << "Dijkstra(A) weights is " << map2string(weights) << endl;
-//   assert(map2string(Weights) == "[B:1][C:4]" && "Dijkstra(A) weights");
-//   // cout << "Dijkstra(A) previous is " << map2string(previous) << endl;
-//   assert(map2string(Previous) == "[B:A][C:B]" && "Dijkstra(A) previous");
-
-//   tie(Weights, Previous) = G.dijkstra("B");
-//   assert(map2string(Weights) == "[A:1][C:3]" && "Dijkstra(B) weights");
-//   assert(map2string(Previous) == "[A:B][C:B]" && "Dijkstra(B) previous");
-
-//   tie(Weights, Previous) = G.dijkstra("X");
-//   assert(map2string(Weights).empty() && "Dijkstra(C) weights");
-//   assert(map2string(Previous).empty() && "Dijkstra(C) previous");
-
-//   Tester::resetSs();
-//   int MstLength = G.mst("A", Tester::edgeVisitor);
-//   assert(MstLength == 4 && "mst A is 4");
-//   assert(Tester::getSs() == "[AB 1][BC 3]" && "mst A is [AB 1][BC 3]");
-
-//   Tester::resetSs();
-//   MstLength = G.mst("B", Tester::edgeVisitor);
-//   assert(MstLength == 4 && "mst 4 is 4");
-//   assert(Tester::getSs() == "[BA 1][BC 3]");
-
-//   Tester::resetSs();
-//   MstLength = G.mst("C", Tester::edgeVisitor);
-//   assert(MstLength == 4 && "mst C is 4");
-//   assert(Tester::getSs() == "[CB 3][BA 1]");
-
-//   Tester::resetSs();
-//   MstLength = G.mst("X", Tester::edgeVisitor);
-//   assert(MstLength == -1 && "mst X is -1");
-//   assert(Tester::getSs().empty() && "mst for vertex not found");
+    //5.1.1
+    Tester::resetSs();
+    string ansStr = "A";
+    G.bfs(ansStr, Tester::labelVisitor);
+    ansStr = "ABC";
+    EXPECT_EQ(Tester::getSs(), ansStr); // starting from A
+    // assert(Tester::getSs() == "ABC" && "starting from A");
 
 
+    //5.1.2
+    Tester::resetSs();
+    ansStr = "B";
+    G.dfs(ansStr, Tester::labelVisitor);
+    ansStr = "BAC";
+    EXPECT_EQ(Tester::getSs(), ansStr); // starting from B
+    //   assert(Tester::getSs() == "BAC" && "starting from B");
+
+
+    //5.1.3
+    Tester::resetSs();
+    ansStr = "C";
+    G.dfs(ansStr, Tester::labelVisitor);
+    ansStr = "CAB";
+    EXPECT_EQ(Tester::getSs(), ansStr); // starting from C
+    //   assert(Tester::getSs() == "CAB" && "starting from C");
+
+
+    //5.1.4
+    Tester::resetSs()
+    ansStr = "X";
+    G.dfs(ansStr, Tester::labelVisitor);
+    EXPECT_TRUE(Tester::getSs().empty()); // starting from X
+    //   assert(Tester::getSs().empty() && "starting from X");
+
+
+
+    // Testing Pt.2.. (also not sure, consider if this can be safely moved to a second test method in this file...)
+
+    //   map<string, int> Weights;
+    //   map<string, string> Previous;
+    //   tie(Weights, Previous) = G.dijkstra("A");
+    //   // cout << "Dijkstra(A) weights is " << map2string(weights) << endl;
+    //   assert(map2string(Weights) == "[B:1][C:4]" && "Dijkstra(A) weights");
+    //   // cout << "Dijkstra(A) previous is " << map2string(previous) << endl;
+    //   assert(map2string(Previous) == "[B:A][C:B]" && "Dijkstra(A) previous");
+
+    //   tie(Weights, Previous) = G.dijkstra("B");
+    //   assert(map2string(Weights) == "[A:1][C:3]" && "Dijkstra(B) weights");
+    //   assert(map2string(Previous) == "[A:B][C:B]" && "Dijkstra(B) previous");
+
+    //   tie(Weights, Previous) = G.dijkstra("X");
+    //   assert(map2string(Weights).empty() && "Dijkstra(C) weights");
+    //   assert(map2string(Previous).empty() && "Dijkstra(C) previous");
+
+    //   Tester::resetSs();
+    //   int MstLength = G.mst("A", Tester::edgeVisitor);
+    //   assert(MstLength == 4 && "mst A is 4");
+    //   assert(Tester::getSs() == "[AB 1][BC 3]" && "mst A is [AB 1][BC 3]");
+
+    //   Tester::resetSs();
+    //   MstLength = G.mst("B", Tester::edgeVisitor);
+    //   assert(MstLength == 4 && "mst 4 is 4");
+    //   assert(Tester::getSs() == "[BA 1][BC 3]");
+
+    //   Tester::resetSs();
+    //   MstLength = G.mst("C", Tester::edgeVisitor);
+    //   assert(MstLength == 4 && "mst C is 4");
+    //   assert(Tester::getSs() == "[CB 3][BA 1]");
+
+    //   Tester::resetSs();
+    //   MstLength = G.mst("X", Tester::edgeVisitor);
+    //   assert(MstLength == -1 && "mst X is -1");
+    //   assert(Tester::getSs().empty() && "mst for vertex not found");
 }
