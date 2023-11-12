0fb68bcd656dab96a852211b8306df7db93fb140
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-11-11 18:47:10 -0800

    Test 2 (Graph0DFS) *SD*
    There may be some degree of less stringent testing occurring in the conversion. see the code comment around line 57-62.
    I think it is correct right now, but if something is not working as expected, I don't have incredibly high confidence at the moment.

--- Tests/Test2.cpp
+++ Tests/Test2.cpp
@@ -1,122 +1,105 @@
 //============================================================================
 // Name        : Test2
-// Test Desc.  : Testing == (detailed) testing and operations
-//				 	(coverage for old test 2 method)
+// Test Desc.  : testing Depth First Search (DFS) on graph 0
+//				 	(coverage for old testGraph0DFS(). was #2 test method...)
 // Author      : Jeffrey Caruso, Yusuf Pisan
 // Date    	   : Fall 2023
 //============================================================================
 
 #include <gtest/gtest.h>
-#include "applib/bst.h"
+#include "applib/graph.h"
 
 #include <iostream>
 #include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
-
-//note / reminder:
-// use EXPECT_EQ (or _NE) for std::string
-// for c strings, use EXPECT_STREQ (or STRNE)
-// http://google.github.io/googletest/reference/assertions.html
-
-
-/**
- * Testing BST - Binary Search Tree functions
- *
- * This file has series of tests for BST
- * Each test is independent and uses assert statements
- * Test functions are of the form
- *
- *      test_netidXX()
- *
- * where netid is UW netid and XX is the test number starting from 01
- *
- * Test functions can only use the public functions from BST
- * testBSTAll() is called from main in main.cpp
- * testBSTAll calls all other functions
- * @author Multiple
- * @date ongoing
- */
 
 /**
  * Trying to avoid global variables,
  * by creating a singleton class with our visitor functions
  * stringstream SS contains the output from visitor
  */
-class TreeVisitor {
+class Tester
+{
 public:
-  // never create an instance of TreeVisitor object
-  // we'll just use the static functions
-  TreeVisitor() = delete;
-
-  // insert output to SS rather than cout, so we can test it
-  static stringstream SS;
-
-  // get SS as a string
-  static string getSS() { return SS.str(); }
-
-  // set SS to be empty string
-  static void resetSS() { SS.str(string()); }
-
-  // instead of cout, insert item into SS, a stringstream object
-  static void visitor(const string &Item) { SS << Item; }
-
-  // instead of cout, insert item into SS, a stringstream object
-  static void visitor(const int &Item) { SS << Item; }
+	Tester() = delete;
+	// insert output to SS rather than cout, so we can test it
+	static stringstream SS;
+	static string getSs() { return SS.str(); }
+	static void resetSs() { SS.str(string()); }
+	// visitor function used for DFS and BFS
+	static void labelVisitor(const string &Label) { SS << Label; }
+	// visitor function used for edges for minimum spanning tree
+	static void edgeVisitor(const string &From, const string &To, int Weight)
+	{
+		SS << "[" << From << To << " " << Weight << "]";
+	}
 };
 
 // initialize the static variable
-//  warning: initialization of 'SS' with static storage duration
-//  may throw an exception that cannot be caught [cert-err58-cpp]
-//  Not sure how to do it without making code harder to read
-//  NOLINTNEXTLINE
-stringstream TreeVisitor::SS;
+// NOLINTNEXTLINE
+stringstream Tester::SS;
 
-template <class T> void visitorSimple(const T &Item) {
-  cout << "visitorSimple: " << Item;
+// convert a map to a string so we can compare it
+template <typename K, typename L>
+static string map2string(const map<K, L> &Mp)
+{
+	stringstream Out;
+	for (auto &P : Mp)
+		Out << "[" << P.first << ":" << P.second << "]";
+	return Out.str();
 }
 
 
 
+/*
+* Note, if testing is not working as expected, there is a decent chance of some degree of misconfigurations.
+* I.E.   assert(G.contains("A") && "a in graph"); -> EXPECT_TRUE(G.contains("A")); // a in graph
+* 				May not be as correct of an interpretation as I am expecting...
+*
+*/
 
 
 //detailed == testing
-TEST(Test2one, DetailedEqualityTesting)
+TEST(Test2, Graph0DFS)
 {
-	BST<string> B1;
-	BST<string> B2;
-	BST<string> B3;
+	Graph G;
+	if (!G.readFile("graph0.txt"))
+		return;
 
-	//add the following to B1
-	for (auto &S : vector<string>{"c", "a", "f", "g", "x"})
-	{
-		B1.add(S);
-	}
+	EXPECT_TRUE(G.contains("A")); // a in graph
+	EXPECT_TRUE(G.contains("B")); // b in graph
+	EXPECT_TRUE(G.contains("C")); // c in graph
+	string ansStr = "B(1),C(8)"
+	EXPECT_EQ(G.getEdgesAsString("A"), ansStr)
+	ansStr = "C(3)";
+	EXPECT_EQ(G.getEdgesAsString("B"), ansStr);
+	EXPECT_TRUE(G.getEdgesAsString("C").empty());
 
-	//add the following to B2
-	for (auto &S : vector<string>{"c", "f", "a", "g", "x"})
-	{
-		B2.add(S);
-	}
+	Tester::resetSs();
+	ansStr = "A";
+	G.dfs(ansStr, Tester::labelVisitor);
+	ansStr = "ABC";
+	EXPECT_EQ(Tester::getSs(), ansStr); // starting from A
 
-	//add "b" to B3
-	B3.add("b");
+	Tester::resetSs();
+	ansStr = "B";
+	G.dfs(ansStr, Tester::labelVisitor);
+	ansStr = "BC";
+	EXPECT_EQ(Tester::getSs(), ansStr); // starting from B
 
-	// == for the 5 node trees
-	EXPECT_TRUE(B1 == B2 && (!(B1 != B2)));
+	Tester::resetSs();
+	ansStr = "C";
+	G.dfs(ansStr, Tester::labelVisitor);
+	ansStr = "C";
+	EXPECT_EQ(Tester::getSs(), ansStr); // starting from C
 
-	//copy constructor usage
-	BST<string> B4(B3);
-	// copy constructor for 1-Node trees B3, B4
-	EXPECT_TRUE(B3 == B4 && (!(B3 != B4)));
+	Tester::resetSs();
+	ansStr = "X";
+	G.dfs(ansStr, Tester::labelVisitor);
+	ansStr = "BC";
+	EXPECT_TRUE(Tester::getSs().empty()); // starting from X
 
-	//copy constructor usage
-	BST<string> B5(B1);
-	// copy constructor for 5-Node trees B1, B5
-	EXPECT_TRUE(B1 == B5 && (!(B5 != B1)));
-
-	//copy constructor usage
-	BST<string> B7("b");
-	// 1-param constructor for 1-Node trees B3, B7
-	EXPECT_TRUE(B3 == B7 && (!(B3 != B7)));
 }
