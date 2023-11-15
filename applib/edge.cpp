dc76c9f7e6eadc1c3926df2b662e8831bb4fab0a
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-11-15 11:21:43 -0800

    trying w/o edge to match Chris's app lib

--- applib/edge.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * Edge is the simplest structure of the graph
- * All edges are directed
- * Each edge belongs to a vertex
- */
-
-#include "edge.h"
-
-Edge::Edge(Vertex *From, Vertex *To, int Weight) {}
