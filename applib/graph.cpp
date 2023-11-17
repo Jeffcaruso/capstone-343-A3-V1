70732e1c62102fb7dac208654e61753659248438
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-11-16 21:22:47 -0800

    fixed duplication in graph.cpp

--- applib/graph.cpp
+++ applib/graph.cpp
@@ -79,48 +79,4 @@ int Graph::mst(const string &StartLabel,
 //                           int Weight)) const {
 //  to w/o that last const...?
 
-bool Graph::disconnect(const string &From, const string &To) { return true; }
-
-void Graph::dfs(const string &StartLabel, void Visit(const string &Label)) {}
-
-void Graph::bfs(const string &StartLabel, void Visit(const string &Label)) {}
-
-// store the weights in a map
-// store the previous label in a map
-pair<map<string, int>, map<string, string>>
-Graph::dijkstra(const string &StartLabel) const
-{
-	map<string, int> Weights;
-	map<string, string> Previous;
-	return make_pair(Weights, Previous);
-}
-
-/**
- * minimum spanning tree
- * @param function to be called on each edge
- * @return length of the minimum spanning tree or -1 if start vertex not found
- */
-int Graph::mst(const string &StartLabel,
-			   void Visit(const string &From, const string &To,
-						  int Weight))
-{
-	// TEST(Test5, testMST)
-	// {
-	//   EXPECT_FALSE(DirectionalEdges);
-	// }
-	// accessing DirectionalEdges was the issue !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	if (!DirectionalEdges)
-	{
-		return -100; // invalid case... Could then use this to do what is needed...
-	}
-	// situation fixed, can now access DirectionalEdges with this...
-	//  assert(!DirectionalEdges);
-	return 0;
-}
-// maybe need to remove const at end of this !!!!!!!!!!!!!!!!!!!!!!!!!
-// int Graph::mst(const string &StartLabel,
-//                void Visit(const string &From, const string &To,
-//                           int Weight)) const {
-//  to w/o that last const...?
-
 // maybe issue here is using assert and not Gtest...?
