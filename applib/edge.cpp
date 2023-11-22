9a87b7756c9a66668fe1692095068b4b60202c12
Jeffcaruso <50027834+Jeffcaruso@users.noreply.github.com>
2023-11-22 08:38:30 -0800

    Seeing if this sorting is what I needed to do

--- applib/edge.cpp
+++ applib/edge.cpp
@@ -44,15 +44,16 @@ bool operator<(Edge &v1, Edge &v2)
     else
     {
         //I think these are correct, but jist in case I got them flipped, lets see...
-        if(strcmp(v1.end.c_str(), v2.end.c_str()) < 0)
+        // v#.end.c_str() ?
+        if(strcmp(v1.start.c_str(), v2.start.c_str()) < 0)
         {
-            //return true;  //expected
-            return false;
+            return true;  //expected
+            //return false;
         }
         else
         {
-            //return false; // expected
-            return true;
+            return false; // expected
+            //return true;
         }
     }
 }
